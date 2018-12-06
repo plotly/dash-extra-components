1cb8803a7f29710e4b6fcecb02c05bc4fb3cc3f5
Philippe Duval <philippe@plot.ly>
2018-12-06 13:01:14 -0500

    Add SuggestionsInput.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
+import SuggestionsInput from './components/SuggestionsInput.react'
 import TableOfContents from './components/TableOfContents.react'
 
 export {
+    SuggestionsInput,
     TableOfContents
 };
